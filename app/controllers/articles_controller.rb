db378a70a64b321181fe110cd31f3828381a92f3
Gilberto Vargas <tachoguitar@gmail.com>
2020-08-25 23:41:44 -0500

    Add paginators

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ActionController::Base
   before_action :find_article, only: %i[show edit update destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 3)
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,11 @@ class UsersController < ActionController::Base
   end
 
   def show
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 3)
   end
 
   def create
