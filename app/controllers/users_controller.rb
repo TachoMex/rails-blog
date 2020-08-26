f868c7dcd80aaa18e7e22d1692ca2911e0eeece0
Gilberto Vargas <tachoguitar@gmail.com>
2020-08-25 22:24:00 -0500

    Added endpoints for editing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ActionController::Base
   layout 'application'
 
+  before_action :find_user, only: %i[show edit update destroy]
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(build_params)
-
     if @user.save
       flash[:notice] = "You signup was successful. Welcome #{@user.username}!"
       redirect_to articles_path
@@ -16,7 +17,22 @@ class UsersController < ActionController::Base
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update(build_params)
+      flash[:notice] = 'Updated correctly'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 
   def build_params
     params.require(:user).permit(:username, :email, :password)
