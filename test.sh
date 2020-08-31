54bea3ddecc49f1bccc27b30fcc1f5995a35411c
Gilberto Vargas <tachoguitar@gmail.com>
2020-08-31 16:07:20 -0500

    Move unit tests on CI to docker

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
-rake test
-bundle exec rubycritic
-bundle exec rubocop
-bundle exec license_finder report --format html > license_report.html
+bundle exec rails db:migrate RAILS_ENV=test
+bundle exec rake assets:precompile RAILS_ENV=Test
+bundle exec rake test
+rubycritic --no-browser -s 85 -p coverage/rubycritic
+rubocop --format html . > coverage/rubocop.html
+license_finder report --format html --decisions-file licenses.yml > coverage/license_report.html
